d01acd914441fe9eba88569f0b230e1498a7d874
Matheus Nunes <matheus.silva@compasso.com.br>
2023-07-03 14:33:10 -0300

    feat: remove try and catch from CreateProductServiceImpl

--- src/app/products/services/CreateProduct/CreateProductServiceImpl.ts
+++ src/app/products/services/CreateProduct/CreateProductServiceImpl.ts
@@ -7,17 +7,13 @@ import { Conflict } from "../../../errors/Conflict";
 export class CreateProductServiceImpl implements CreateProductService {
     constructor(
         private readonly productRepository: ProductRepository,
-        private next: NextFunction
     ) {}
     async execute(product: CreateProductDTO): Promise<any> {
         const { name } = product
-        try {
-            const productExists = await this.productRepository.findAll({ name })
-            if(productExists) throw new Conflict(`The product ${name} already exists`)
 
-            return this.productRepository.create(product)
-        } catch(error) {
-            this.next(error)
-        }
+        const productExists = await this.productRepository.findAll({ name })
+        if(productExists) throw new Conflict(`The product ${name} already exists`)
+
+        return this.productRepository.create(product)
     }
 }
