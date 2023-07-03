ec7f1779e2baaf038869a395f9322f7629c60957
Matheus Nunes <matheus.silva@compasso.com.br>
2023-07-03 13:23:17 -0300

    feat: create error middleware

---
+++ src/app/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,10 @@
+import {Request, Response, NextFunction} from 'express'
+
+export const errorMiddleware = (error: any, req: Request, res: Response, next: NextFunction) => {
+    let statusCode: number = 500;
+
+    if (error instanceof NotFound) statusCode = 404;
+    else statusCode = 400;
+
+    res.status(statusCode).json(statusCode !== 500 ? error : 'Something went wrong');
+};
