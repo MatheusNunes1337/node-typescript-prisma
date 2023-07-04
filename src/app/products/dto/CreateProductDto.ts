ea9dd3aec60f4592a4ac8fa2af194a6a8856d29e
Matheus Nunes <matheus.silva@compasso.com.br>
2023-07-04 14:50:44 -0300

    feat: add validator to CreateProductDto

--- src/app/products/dto/CreateProductDto.ts
+++ src/app/products/dto/CreateProductDto.ts
@@ -1,7 +1,27 @@
-export interface CreateProductDTO {
-    name: string,
-    description: string,
-    price: number,
-    stock: number,
-    coverImage: string
+import {IsString, Length, IsDecimal, 
+    IsNumber, IsOptional, Min, IsNotEmpty,
+} from 'class-validator'
+
+export class CreateProductDTO {
+    @IsString()
+    @Length(3, 45)
+    @IsNotEmpty()
+    name!: string;
+
+    @IsString()
+    @Length(5, 100)
+    @IsNotEmpty()
+    description!: string;
+
+    @IsDecimal()
+    @Min(0)
+    price!: number;
+
+    @IsNumber()
+    @Min(0)
+    stock!: number;
+
+    @IsString()
+    @IsOptional()
+    coverImage!: string;
 }
