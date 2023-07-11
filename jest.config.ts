95957cd2d4ee2afc65bd3838c2b2ab7f504f8844
Matheus Nunes <matheus.silva@compasso.com.br>
2023-07-11 12:04:03 -0300

    feat: set prisma mock module

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config.InitialOptions = {
   testPathIgnorePatterns: [
     "/__tests__/factories/"
   ],
+  setupFilesAfterEnv: ['<rootDir>/singleton.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
 
